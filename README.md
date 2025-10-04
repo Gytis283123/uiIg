f0069dbc73f0faa52aede45931282c8fbbc5e387
Gae123 <gytisgytis053@gmail.com>
2025-10-04 14:28:06 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# My UI Project
+
+View the live interactive UI here: [Live Demo](https://Gytis283123.github.io/uiIg/)
---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("clickMe").addEventListener("click", () => {
+  alert("You clicked the first button!");
+});
+
+document.getElementById("helloBtn").addEventListener("click", () => {
+  alert("Hello from the Galaxy UI!");
+});
